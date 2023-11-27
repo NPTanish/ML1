a766d668e6a5b269807c67644f42c3d00ceb97cc
NPTanish <vishnoipree1@gmail.com>
2023-11-27 09:49:37 -0600

    EDA Melbourne Data

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template
+import numpy as np 
+import pandas as pd 
+
+from sklearn.preprocessing import StandardScaler
+application = Flask(__name__)
+
+app = application
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/predictdata",methods = ['GET','POST'])
+def predict_datapoint():
+    
